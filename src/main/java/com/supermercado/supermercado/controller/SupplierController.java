226d48ee8d4ddcdae7ba76baf257ac7cc69860c6
bryan-c-u <uranpavabryancamilo@gmail.com>
2026-04-08 07:09:45 -0500

    feat : creacion de el dto StockEntry para la realización de el endpoint entrada almacen

--- src/main/java/com/supermercado/supermercado/controller/SupplierController.java
+++ src/main/java/com/supermercado/supermercado/controller/SupplierController.java
@@ -60,6 +60,4 @@ public class SupplierController {
         return ResponseEntity.ok(supplierService.getByNit(nit));
     }
 
-
-   
 }
---
+++ src/main/java/com/supermercado/supermercado/entity/StockEntryDTO.java
@@ -0,0 +1,18 @@
+package com.proveedores_almacen.proveedores_almacen.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class StockEntryDTO {
+
+    @NotNull(message = "Product ID is required")
+    private Long productId;
+
+    @NotNull(message = "Supplier ID is required")
+    private Long supplierId;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private Integer quantity;
+}
