d68f3ee5f7bf797401b4a3b2407351d6241febe8
rixhard-rxp <oskitarmolpa@gmail.com>
2026-04-07 21:24:47 -0500

    feat: Agregué comentado la relación many to many que necesita Bryan

--- src/main/java/com/supermercado/supermercado/entity/Product.java
+++ src/main/java/com/supermercado/supermercado/entity/Product.java
@@ -16,9 +16,14 @@ public class Product {
     private Double cost;
 
     private Integer stock; // Para la Regla de Negocio de validación
-    
+
     @Column(unique = true, nullable = false)
     private String barcode; // Regla de Negocio 2
 
     private Boolean active = true;
+
+    //@ManyToMany
+    //@JoinTable(name = "producto_proveedor", joinColumns = @JoinColumn(name = "producto_id"), inverseJoinColumns = @JoinColumn(name = "proveedor_id"))
+    //private List<Supplier> suppliers;
+    // Para Bryan
 }
